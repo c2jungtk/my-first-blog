59d034fc991d727da58a7939b04ffc6f24b070bf
Jung TK <manlyion@naver.com>
2018-02-07 09:49:49 +0900

    템플릿 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
+
+
+class Postadmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'published_date')
+
 # Register your models here.
 
-admin.site.register(Post)
+admin.site.register(Post, Postadmin)
