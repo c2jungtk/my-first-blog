0c9f82d2b4153f5bd6d2340c1fa182cfe5a2b0b7
Jung TK <manlyion@naver.com>
2018-02-08 14:04:48 +0900

    블로그 완료

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
@@ -30,4 +32,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+
 
