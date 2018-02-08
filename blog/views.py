6f9bbd0073fd27c335b7567d6412e29038c450b1
Jung TK <manlyion@naver.com>
2018-02-08 14:26:35 +0900

    블로그완료 오류 수정

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from blog.models import Post, Comment
 from django.utils import timezone
 from blog.forms import PostForm, CommnetForm
 # Create your views here.
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
