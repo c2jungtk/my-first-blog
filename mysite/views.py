e0bec6651d82a3a0135dd31d3e34ca8dcea02fe4
Jung TK <manlyion@naver.com>
2018-02-12 12:45:28 +0900

    회원가입 적용시키기

--- mysite/views.py
+++ mysite/views.py
@@ -4,6 +4,14 @@ from blog.models import Post
 from board.models import Board
 from django.shortcuts import render
 
+
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+
+
+
 def home(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body><html>"% now
@@ -11,3 +19,11 @@ def home(request):
     boards = Board.objects.order_by('-created_date')[0:5]
 
     return render(request, 'home.html', {'posts': posts, 'boards': boards} )
+
+class UserRegister(CreateView):
+    template_name = "registration/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+class UserRegisterDone(TemplateView):
+    template_name = "registration/register_done.html"
